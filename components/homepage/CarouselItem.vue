3d000e40d1d29c760300ac92a040171aa4a08ce4
Carine Dengler <git@carine-dengler.de>
2024-08-13 17:37:35 +0200

    feat: create homepage top part

---
+++ components/homepage/CarouselItem.vue
@@ -0,0 +1,50 @@
+<script>
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faUserSecret);
+</script>
+
+<template>
+  <div
+    class="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md border border-blue-gray-100"
+  >
+    <img
+      alt="Dela"
+      src="https://dedis.github.io/dela/assets/logo.png"
+      class="pt-4 pr-4 pl-4 object-cover w-full h-auto"
+    >
+    <div
+      class="relative bg-clip-border mt-4 mx-4 rounded-xl overflow-hidden bg-transparent text-gray-700 shadow-none !m-0 p-6"
+    >
+      <h5
+        class="block antialiased tracking-normal font-sans text-xl font-semibold leading-snug text-blue-gray-900 capitalize"
+      >
+        DEDIS Ledger Architecture
+      </h5>
+      <p
+        class="block antialiased font-sans text-sm leading-normal text-inherit font-normal !text-gray-500"
+      >
+        Dela stands for DEDIS Ledger Architecture. It is both a set of
+        abstractions and an implementation of a distributed ledger architecture.
+      </p>
+    </div>
+    <div class="flex space-x-4 justify-end pb-4 pr-4">
+      <a href="https://github.com/dedis/dela/blob/main/docs">
+        <div class="text-right">
+          <font-awesome :icon="['fa', 'newspaper']" class="fa-3x" />
+        </div>
+      </a>
+      <a href="https://dedis.github.io/dela/#/">
+        <div class="text-right">
+          <font-awesome :icon="['fa', 'home']" class="fa-3x" />
+        </div>
+      </a>
+      <a href="https://github.com/dedis/dela">
+        <div class="text-right">
+          <font-awesome :icon="['fab', 'github']" class="fa-3x" />
+        </div>
+      </a>
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="bg-white">
+    <div class="relative isolate px-6 pt-14 lg:px-8">
+      <div class="mx-auto max-w-10xl">
+        <div class="text-center">
+          <h1
+            class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl"
+          >
+            Welcome to the Center for Digital Trust's Showcase
+          </h1>
+          <p class="mt-6 text-lg leading-8 text-gray-600">
+            This page presents all projects from the labs affiliated to the
+            Center for Digital Trust. You can filter the projects according to
+            your preferences. Clicking on one of the projects will show an
+            in-depth description.
+          </p>
+        </div>
+      </div>
+    </div>
+    <section class="px-56 py-24">
+      <div class="mx-auto max-w-10xl">
+        <div class="text-center">
+          <h2
+            class="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl"
+          >
+            Selected Projects
+          </h2>
+        </div>
+      </div>
+      <div class="mt-24">
+        <div class="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
+          <HomepageCarouselItem />
+          <HomepageCarouselItem />
+          <HomepageCarouselItem />
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
