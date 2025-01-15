7b8c860f6e5046cf67454fa4d00f3d3c887c95b3
Ahmed Elghareeb <ahmed.elghareeb@proton.me>
2025-01-15 11:11:01 +0200

    style: automated adjustments to style from VSCode

--- utils/loadData.ts
+++ utils/loadData.ts
@@ -15,24 +15,24 @@ const PROJECTS_FILE = 'projects.yaml';
 const LABS_FILE = 'labs.yaml'
 
 function validateData(basename, content) {
-    const ajv = new Ajv({ allowUnionTypes: true });
-    var schema;
-    switch (basename) {
-      case LABS_FILE:
-        schema = ajv.compile(labsSchema);
-        break;
-      case PROJECTS_FILE:
-        schema = ajv.compile(projectsSchema);
-        break;
-      default:
-        throw(`unsupported file '${basename}'`);
-    }
-    if (!schema(content)) {
-      throw(`invalid YAML: ${JSON.stringify(schema.errors)}`);
-    }
+  const ajv = new Ajv({ allowUnionTypes: true });
+  var schema;
+  switch (basename) {
+    case LABS_FILE:
+      schema = ajv.compile(labsSchema);
+      break;
+    case PROJECTS_FILE:
+      schema = ajv.compile(projectsSchema);
+      break;
+    default:
+      throw (`unsupported file '${basename}'`);
+  }
+  if (!schema(content)) {
+    throw (`invalid YAML: ${JSON.stringify(schema.errors)}`);
+  }
 }
 
-export function loadLabs(skipValidation: boolean = false){
+export function loadLabs(skipValidation: boolean = false) {
   const filePath = path.join(DATADIR, LABS_FILE)
   console.log(`processing ${filePath}`);
   const content = YAML.parse(fs.readFileSync(filePath, 'utf-8'));
@@ -43,7 +43,7 @@ export function loadLabs(skipValidation: boolean = false){
   return content;
 }
 
-export function loadProjects(skipValidation: boolean = false){
+export function loadProjects(skipValidation: boolean = false) {
   const labProjects = fs.readdirSync(DATADIR, { withFileTypes: true }).filter(file => file.isDirectory()).map((file) => {
     const filePath = path.join(file.parentPath, file.name, PROJECTS_FILE);
     console.log(`processing ${filePath}`);
