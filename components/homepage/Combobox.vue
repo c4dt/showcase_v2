e50097f0e774cda35d9e06d26e80adf8facf59be
Ahmed Elghareeb <ahmed.elghareeb@proton.me>
2025-01-22 11:28:03 +0100

    feat: implement filter component
    - Use props to fill dropdown
    - Implement search within dropdown
    - Close component when an item selected
    - Fill input field with selected item

--- components/homepage/Combobox.vue
+++ components/homepage/Combobox.vue
@@ -1,23 +1,49 @@
 <script setup lang="ts">
-import { ref } from "vue";
-const filteredOptions = ref<string[]>(["DEDIS", "DLAB", "LSIR"]);
 const isOpen = ref(false);
 function toggleDropdown() {
-  isOpen.value = !isOpen.value;
+  if (isOpen.value) {
+    isOpen.value = false;
+  } else {
+    isOpen.value = true;
+    fieldValue.value = "";
+  }
 }
-defineProps<{
+const props = defineProps<{
   title: string;
+  itemList: string[];
 }>();
+
+const fieldValue = ref("");
+
+const filteredList = computed(() => {
+  return props.itemList.filter((name) => name.toLowerCase().includes(fieldValue.value.toLowerCase()));
+});
+
+function selectItem(name: string) {
+  fieldValue.value = name;
+  isOpen.value = false;
+}
 </script>
 
 <template>
-  <div class="relative py-2">
-    <input type="text" :placeholder="title" class="w-full px-4 py-2 pr-8 border border-gray-300 rounded-md" />
+  <div class="dropdown-container relative py-2">
+    <input
+      type="text"
+      v-model="fieldValue"
+      :placeholder="title"
+      class="w-full px-4 py-2 pr-8 border border-gray-300 rounded-md"
+      @focusin="isOpen = true"
+    />
     <button class="absolute right-0 px-2 py-2 text-gray-500 text-center" @click="toggleDropdown">
       <font-awesome :icon="['fa', 'caret-down']" />
     </button>
     <ul v-if="isOpen" class="absolute z-10 w-full mt-1 bg-white border border-gray-300 rounded-md shadow-lg">
-      <li v-for="name in filteredOptions" class="px-4 py-2 cursor-pointer hover:bg-gray-100">
+      <li
+        v-for="name in filteredList"
+        :key="name"
+        @click="selectItem(name)"
+        class="px-4 py-2 cursor-pointer hover:bg-gray-100"
+      >
         {{ name }}
       </li>
     </ul>
