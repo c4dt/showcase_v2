39d4e073bf40df2dd0a9c93832844b5265f49d15
Ahmed Elghareeb <ahmed.elghareeb@proton.me>
2025-02-19 17:12:39 +0100

    feat: enables search mode when using one of the search boxes

--- app.vue
+++ app.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+const route = useRoute();
+const searchQuery = ref<string>(typeof route.query.search === "string" ? route.query.search : "");
+
+provide("searchQuery", searchQuery);
+</script>
 <template>
   <div>
     <Navbar />
