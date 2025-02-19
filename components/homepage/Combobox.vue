e7c81a7e2161ca49dc9a0b1671a348e7f4f1f57d
Ahmed Elghareeb <ahmed.elghareeb@proton.me>
2025-02-19 17:09:49 +0100

    refactor: improves how combobox opens and closes
    -click inside: open
    -click outside: close
    -click on item:
       - Mouse down: select item
       - Mouse up: close

--- components/homepage/Combobox.vue
+++ components/homepage/Combobox.vue
@@ -18,24 +18,22 @@ function selectItem(name: string) {
 </script>
 
 <template>
-  <div class="dropdown-container relative py-2">
+  <div class="dropdown-container relative py-2" @focusin="isOpen = true" @focusout="isOpen = false">
     <input
       v-model="selectedItem"
       type="text"
       :placeholder="title"
       class="w-full px-4 py-2 pr-8 border border-gray-300 rounded-md"
-      @focusin="isOpen = true"
     />
     <button class="absolute right-0 px-2 py-2 text-gray-500 text-center" @click="isOpen = !isOpen">
       <font-awesome :icon="['fa', 'caret-down']" />
     </button>
     <ul v-if="isOpen" class="absolute z-10 w-full mt-1 bg-white border border-gray-300 rounded-md shadow-lg">
-      <li class="px-4 py-2 cursor-pointer hover:bg-gray-100" @click="selectItem('')">All</li>
       <li
         v-for="name in filteredList"
         :key="name"
         class="px-4 py-2 cursor-pointer hover:bg-gray-100"
-        @click="selectItem(name)"
+        @mousedown.prevent="selectItem(name)"
       >
         {{ name }}
       </li>
