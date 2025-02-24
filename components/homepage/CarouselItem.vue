7309605e828c15ac81df1021bb018eddf579a1a5
Ahmed Elghareeb <ahmed.elghareeb@proton.me>
2025-02-24 16:38:15 +0100

    feat: makes carousel items clickable and link to project

--- components/homepage/CarouselItem.vue
+++ components/homepage/CarouselItem.vue
@@ -27,17 +27,19 @@ const truncatedDescription = computed(() => {
 
 <template>
   <div class="relative flex flex-col rounded-xl bg-white text-gray-700 shadow-md border border-blue-gray-100">
-    <div class="flex items-center justify-center h-1/2">
-      <img :alt="project.name" :src="project.logo" class="p-4 object-cover h-full w-auto" />
-    </div>
-    <div class="mx-2 p-4 rounded-xl text-gray-700">
-      <h5 class="antialiased font-sans text-xl font-semibold leading-snug text-blue-gray-900 capitalize">
-        {{ project.name }}
-      </h5>
-      <p class="antialiased font-sans text-sm leading-normal text-gray-500">
-        {{ truncatedDescription }}
-      </p>
-    </div>
+    <NuxtLink :to="{ name: 'projects-id', params: { id: project.id } }">
+      <div class="flex items-center justify-center h-1/2">
+        <img :alt="project.name" :src="project.logo" class="p-4 object-cover h-full w-auto" />
+      </div>
+      <div class="mx-2 p-4 rounded-xl text-gray-700">
+        <h5 class="antialiased font-sans text-xl font-semibold leading-snug text-blue-gray-900 capitalize">
+          {{ project.name }}
+        </h5>
+        <p class="antialiased font-sans text-sm leading-normal text-gray-500">
+          {{ truncatedDescription }}
+        </p>
+      </div>
+    </NuxtLink>
     <div class="flex space-x-4 justify-end pb-4 pr-4 mt-auto">
       <a v-if="project.information" :href="project.information[0].url">
         <font-awesome :icon="['fa', 'newspaper']" class="fa-3x" />
