b3c6daae54b9f9957a5489728c947a45d992cf43
Ahmed Elghareeb <ahmed.elghareeb@proton.me>
2025-02-24 14:57:22 +0100

    fix: adjusts projects page classes to work with tailwindcss v4

--- components/projects/Tabs.vue
+++ components/projects/Tabs.vue
@@ -1,62 +1,45 @@
 <template>
-  <div class="container">
-    <div class="header">
+  <div class="container py-4">
+    <div class="header flex">
       <button
         v-for="tab in tabs"
-        :class="{ 'tab': true, active: activeTab === tab.id }"
+        :class="{
+          'bg-[#ff0000] px-4 py-2 text-[#e6e6e6]': true,
+          'bg-[#ffffff] rounded-t-xl text-[#212121] border-b-[#ff0000] border-solid border-b-2': activeTab === tab.id
+        }"
         @click="useRouter().replace({ hash: `#${tab.id}` })"
       >
         {{ tab.label }}
       </button>
     </div>
-    <div class="content">
-      <div v-html="tabs.find((tab) => tab.id === activeTab).content"></div>
+    <div class="text-left py-2 bg-[#ffffff]">
+      <div v-html="tabs.find((tab) => tab.id === activeTab).content" />
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-  const props = defineProps<{
-    tabs: {
-      id: string;
-      label: string;
-      content: string;
-    }[];
-    defaultTab: string;
-  }>();
-  const tabIds = props.tabs.map((tab) => { return tab.id });
-  const route = useRoute();
-  const currTab = route.hash.slice(1);
-  const activeTab = ref(tabIds.includes(currTab) ? currTab : props.defaultTab);
-  watch(
-    () => route.hash,
-    (hash) => {
-      const tabId = hash.slice(1);
-      if (tabIds.includes(tabId)) {
-        activeTab.value = tabId;
-      }
+const props = defineProps<{
+  tabs: {
+    id: string;
+    label: string;
+    content: string;
+  }[];
+  defaultTab: string;
+}>();
+const tabIds = props.tabs.map((tab) => {
+  return tab.id;
+});
+const route = useRoute();
+const currTab = route.hash.slice(1);
+const activeTab = ref(tabIds.includes(currTab) ? currTab : props.defaultTab);
+watch(
+  () => route.hash,
+  (hash) => {
+    const tabId = hash.slice(1);
+    if (tabIds.includes(tabId)) {
+      activeTab.value = tabId;
     }
-  );
+  }
+);
 </script>
-
-<style scoped>
-.container {
-  @apply py-4
-}
-
-.header {
-  @apply flex
-}
-
-.tab {
-  @apply bg-[#ff0000] px-4 py-2 text-[#e6e6e6]
-}
-
-.active {
-  @apply bg-[#ffffff] rounded-t-xl text-[#212121] border-b-[#ff0000] border-solid border-b-2
-}
-
-.content {
-  @apply text-left py-2 bg-[#ffffff]
-}
-</style>
--- pages/projects/[id].vue
+++ pages/projects/[id].vue
@@ -1,60 +1,94 @@
 <script setup lang="ts">
-  const { data: projects } = await useFetch("/api/projects");
-  const project = projects.value.find((project) => project.id == useRoute().params.id);
-  const { data: labs } = await useFetch("/api/labs");
-  const lab = labs.value.labs[project.lab];
-  const { data: presentation } = await useFetch(`/api/templates?id=${project.id}&type=presentation`);
-  const { data: app } = await useFetch(`/api/templates?id=${project.id}&type=app`);
-  const { data: demo } = await useFetch(`/api/templates?id=${project.id}&type=demo`);
-  const { data: details } = await useFetch(`/api/templates?id=${project.id}&type=details`);
-  const { data: handsOn } = await useFetch(`/api/templates?id=${project.id}&type=hands-on`);
-  const { data: pilot } = await useFetch(`/api/templates?id=${project.id}&type=pilot`);
-  const tabs = [
-    { id: "presentation", label: "Presentation", content: presentation.value },
-    { id: "app", label: "App", content: app.value },
-    { id: "demo", label: "Demo", content: demo.value },
-    { id: "details", label: "Details", content: details.value },
-    { id: "hands-on", label: "Hands-on", content: handsOn.value },
-    { id: "pilot", label: "Pilot", content: pilot.value }
-  ].filter((tab) => tab.content)
-  const defaultTab = tabs.length ? tabs[0].id : null;
-  const lastEdited = new Date(Date.parse(project.date_updated ? project.date_updated : project.date_added));
-  const status = computed(() => {
-    return project.code && project.code.date_last_commit ? (isActive(project.code.date_last_commit) ? "active" : "inactive") : "unknown";
-  });
-  const articles = project.information ? project.information.filter((information) => information.type.toLowerCase() === "article") : [];
-  const papers = project.information ? project.information.filter((information) => information.type.toLowerCase() === "paper") : [];
+const { data: projects } = await useFetch("/api/projects");
+const project = projects.value.find((project) => project.id == useRoute().params.id);
+const { data: labs } = await useFetch("/api/labs");
+const lab = labs.value.labs[project.lab];
+const { data: presentation } = await useFetch(`/api/templates?id=${project.id}&type=presentation`);
+const { data: app } = await useFetch(`/api/templates?id=${project.id}&type=app`);
+const { data: demo } = await useFetch(`/api/templates?id=${project.id}&type=demo`);
+const { data: details } = await useFetch(`/api/templates?id=${project.id}&type=details`);
+const { data: handsOn } = await useFetch(`/api/templates?id=${project.id}&type=hands-on`);
+const { data: pilot } = await useFetch(`/api/templates?id=${project.id}&type=pilot`);
+const tabs = [
+  { id: "presentation", label: "Presentation", content: presentation.value },
+  { id: "app", label: "App", content: app.value },
+  { id: "demo", label: "Demo", content: demo.value },
+  { id: "details", label: "Details", content: details.value },
+  { id: "hands-on", label: "Hands-on", content: handsOn.value },
+  { id: "pilot", label: "Pilot", content: pilot.value }
+].filter((tab) => tab.content);
+const defaultTab = tabs.length ? tabs[0].id : null;
+const lastEdited = new Date(Date.parse(project.date_updated ? project.date_updated : project.date_added));
+
+let projectStatus: { StyleClass: string; text: string };
+projectStatus = computed(() => {
+  if (project.code && project.code.date_last_commit) {
+    if (isActive(project.code.date_last_commit)) {
+      return {
+        StyleClass:
+          "px-3 py-1 bg-green-200 text-green-800  border-green-500 border-solid border-1px rounded-full text-sm",
+        text: "Active"
+      };
+    } else {
+      return {
+        StyleClass: "px-3 py-1 bg-red-200 text-red-800 border-red-500 border-solid border-1px rounded-full text-sm",
+        text: "Inactive"
+      };
+    }
+  } else {
+    return {
+      StyleClass: "px-3 py-1 bg-gray-200 text-gray-800 border-gray-500 border-solid border-1px rounded-full text-sm",
+      text: "Unknown"
+    };
+  }
+});
+
+const articles = project.information
+  ? project.information.filter((information) => information.type.toLowerCase() === "article")
+  : [];
+const papers = project.information
+  ? project.information.filter((information) => information.type.toLowerCase() === "paper")
+  : [];
 </script>
 <template>
   <div class="flex m-16">
     <div class="pr-16 flex-[8] text-center divide-y divide-solid">
       <div class="py-4">
-        <h1 class="header-h1">{{ project.name }}</h1>
+        <h1 class="text-4xl font-bold">{{ project.name }}</h1>
         <p class="text-xs py-4">This page was last edited on {{ lastEdited.toDateString() }}.</p>
-        <span :class="status">{{ status }}</span>
+        <span :class="projectStatus.StyleClass">{{ projectStatus.text }}</span>
       </div>
       <div class="py-4">
         <p class="text-left py-4">{{ project.descriptionDisplay }}</p>
         <div class="flex space-x-4 text-left">
-          <span
-            class="px-3 py-1 rounded-full text-sm bg-[#d5d5d5] text-[#707070]"
-            v-for="tag in project.tags"
-          >
+          <span class="px-3 py-1 rounded-full text-sm bg-[#d5d5d5] text-[#707070]" v-for="tag in project.tags">
             {{ tag }}
           </span>
         </div>
       </div>
       <div class="py-4 text-left">
         <div v-if="papers.length">
-          <h2 class="header-h2">Papers</h2>
+          <h2 class="text-2xl font-bold">Papers</h2>
           <ul class="ul">
-            <li v-for="paper in papers"><a :href="paper.url" class="link">{{ paper.title }}</a></li>
+            <li v-for="paper in papers">
+              <a
+                :href="paper.url"
+                class="underline text-[#212121] hover:text-[#ff0000] decoration-[#ff0000] hover:decoration-[#212121]"
+                >{{ paper.title }}</a
+              >
+            </li>
           </ul>
         </div>
         <div v-if="articles.length">
-          <h2 class="header-h2">Articles</h2>
+          <h2 class="text-2xl font-bold">Articles</h2>
           <ul class="ul">
-            <li v-for="article in articles"><a :href="article.url" class="link">{{ article.title }}</a></li>
+            <li v-for="article in articles">
+              <a
+                :href="article.url"
+                class="underline text-[#212121] hover:text-[#ff0000] decoration-[#ff0000] hover:decoration-[#212121]"
+                >{{ article.title }}</a
+              >
+            </li>
           </ul>
         </div>
       </div>
@@ -63,11 +97,19 @@
     <div class="flex-[2]">
       <div class="px-16 bg-[#d5d5d5] text-center divide-y divide-solid">
         <div class="py-4">
-          <h1 class="header-h1">{{ project.lab }}</h1>
-          <a class="link text-sm" :href="lab.url">{{ lab.name }}</a>
+          <h1 class="text-4xl font-bold">{{ project.lab }}</h1>
+          <a
+            class="underline text-[#212121] hover:text-[#ff0000] decoration-[#ff0000] hover:decoration-[#212121] text-sm"
+            :href="lab.url"
+            >{{ lab.name }}</a
+          >
           <div>
             <p class="text-center py-4 text-l">
-            <a class="link text-xl" :href="'mailto:' + lab.prof.email">Prof. {{ lab.prof.name.join(" ") }}</a><br/>
+              <a
+                class="underline text-[#212121] hover:text-[#ff0000] decoration-[#ff0000] hover:decoration-[#212121] text-xl"
+                :href="'mailto:' + lab.prof.email"
+                >Prof. {{ lab.prof.name.join(" ") }}</a
+              ><br />
             </p>
           </div>
         </div>
@@ -78,31 +120,7 @@
 </template>
 
 <style scoped>
-@import "./assets/css/products.css";
-
-.header-h1 {
-  @apply text-4xl font-bold
-}
-
-.header-h2 {
-  @apply text-2xl font-bold
-}
-
-.link {
-  @apply underline text-[#212121] hover:text-[#ff0000] decoration-[#ff0000] hover:decoration-[#212121]
-}
-
-.active {
-  @apply px-3 py-1 bg-green-200 text-green-800  border-green-500 border-solid border-[1px] rounded-full text-sm;
-}
-
-.inactive {
-  @apply px-3 py-1 bg-red-200 text-red-800 border-red-500 border-solid border-[1px] rounded-full text-sm;
-}
-
-.unknown {
-  @apply px-3 py-1 bg-gray-200 text-gray-800  border-gray-500 border-solid border-[1px] rounded-full text-sm;
-}
+@import "assets/css/products.css";
 
 .ul {
   list-style-type: square;
