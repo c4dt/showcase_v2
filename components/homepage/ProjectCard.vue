0706c2ed2002bbfce183164aee4b714451deecdb
Ahmed Elghareeb <ahmed.elghareeb@proton.me>
2025-03-10 13:13:05 +0100

    refactor: removes testing information

--- components/homepage/ProjectCard.vue
+++ components/homepage/ProjectCard.vue
@@ -16,7 +16,7 @@
         <div class="flex items-start">
           <!-- Left Side: Project Name and Description -->
           <div class="grow">
-            <h2 class="text-2xl font-bold mb-2">{{ project.name }} - {{ project.status }}</h2>
+            <h2 class="text-2xl font-bold mb-2">{{ project.name }}</h2>
             <h3>{{ project.description }}</h3>
             <p class="text-gray-600 mb-4">{{ project.laymen_desc || project.tech_desc }}</p>
             <h3>Professor: {{ project.lab.prof.name.join(" ") }}</h3>
