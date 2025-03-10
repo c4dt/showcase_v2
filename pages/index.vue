70df57dc0ec9e25e0044a5ea71eb95f975c3a2ec
Ahmed Elghareeb <ahmed.elghareeb@proton.me>
2025-03-10 13:01:28 +0100

    refactor: removes outer borders on the search box

--- pages/index.vue
+++ pages/index.vue
@@ -196,24 +196,22 @@ const projectsToDisplay = computed<ExtendedProject[]>(() => {
         <!-- Main projects area -->
         <div class="w-full md:w-3/4 mt-8 md:mt-0">
           <!-- Search -->
-          <div class="md:sticky border top-0 mb-4 bg-white rounded-xl shadow-md py-2 px-6">
-            <div class="relative">
-              <input
-                v-model="searchQuery"
-                type="text"
-                placeholder="Enter a project name..."
-                class="w-full py-2 pl-10 pr-4 text-gray-700 bg-gray-200 rounded-full focus:outline-hidden focus:bg-white focus:ring-2 focus:ring-blue-300"
-              />
-              <div class="absolute inset-y-0 left-0 flex items-center pl-3">
-                <svg class="w-5 h-5 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                  />
-                </svg>
-              </div>
+          <div class="relative">
+            <input
+              v-model="searchQuery"
+              type="text"
+              placeholder="Enter a project name..."
+              class="w-full py-2 pl-10 pr-4 mb-4 text-gray-700 bg-gray-200 rounded-full focus:outline-hidden focus:bg-white focus:ring-2 focus:ring-blue-300"
+            />
+            <div class="absolute inset-y-0 left-0 flex items-center pl-3">
+              <svg class="w-5 h-5 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
             </div>
           </div>
           <div v-for="project in projectsToDisplay" :key="project.name" class="py-2">
