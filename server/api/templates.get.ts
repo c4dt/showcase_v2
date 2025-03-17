dbf5302d6db9540301461329c5f6829be6597151
Ahmed Elghareeb <ahmed.elghareeb@proton.me>
2025-03-17 18:14:16 +0100

    refactor: uses new templates API and deletes old one

--- server/api/templates.get.ts
+++
@@ -1,6 +0,0 @@
-import { loadTemplate } from "@/utils/loadData";
-
-export default defineEventHandler((event) => {
-  const query = getQuery(event);
-  return loadTemplate(query.id, query.type);
-});
