3334557079d3ad0511bbf27a0c6a534724a7bce3
Ahmed Elghareeb <ahmed.elghareeb@proton.me>
2025-03-24 15:16:24 +0100

    refactor: improves typing and fixes linting issues

--- utils/misc.ts
+++ utils/misc.ts
@@ -1,8 +1,8 @@
 const THRESHOLD_DAYS = 270;
 const THRESHOLD = THRESHOLD_DAYS * 1000 * 60 * 60 * 24;
 
-export function isActive(dateLastCommit) {
-  return Date() - Date.parse(dateLastCommit) <= THRESHOLD;
+export function isActive(dateLastCommit: string) {
+  return Date.now() - Date.parse(dateLastCommit) <= THRESHOLD;
 }
 
 export function getLabel(word: string) {
