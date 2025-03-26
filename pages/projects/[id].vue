1e4b269c85d7a6613bdb8fcd8de9880b446cd58a
Carine Dengler <git@carine-dengler.de>
2025-03-26 15:59:58 +0100

    feat: add 'Go back' button to project page

--- pages/projects/[id].vue
+++ pages/projects/[id].vue
@@ -56,60 +56,69 @@ tabs = [
 const lastEdited = new Date(Date.parse(project.value.date_updated || project.value.date_added));
 </script>
 <template>
-  <div class="flex m-16">
-    <div class="pr-16 flex-[7] text-center">
-      <div class="py-4">
-        <p class="text-xs py-4">This page was last edited on {{ lastEdited.toDateString() }}.</p>
-        <div class="flex items-center justify-center">
-          <img :alt="project.name" :src="project.logo" class="p-4 object-contain w-full h-48" />
-        </div>
-        <ProjectStatus :project="project" />
-        <ProjectMaturity class="flex justify-end px-4" :maturity="project.maturity ? project.maturity : 0" />
-        <ProjectDescription :project="project" />
-        <div class="flex items-center space-x-2 text-left">
-          <FontAwesomeIcon :icon="faTags" class="text-gray-500" />
-          <span
-            v-for="tag in project.tags"
-            :key="tag"
-            class="px-3 py-1 rounded-full text-sm bg-[#d5d5d5] text-[#707070]"
-          >
-            {{ tag }}
-          </span>
-        </div>
-      </div>
-      <ProjectsTabs v-if="tabs.length" :tabs="tabs" />
-    </div>
-    <div class="flex-[3]">
-      <div class="px-8 bg-[#e6e6e6] text-center">
+  <div>
+    <div class="flex m-16">
+      <div class="pr-16 flex-[7] text-center">
         <div class="py-4">
-          <h1 class="text-4xl font-bold">{{ project.lab.name }}</h1>
-          <a
-            class="underline text-[#212121] hover:text-[#ff0000] decoration-[#ff0000] hover:decoration-[#212121] text-sm"
-            :href="lab.url"
-            >{{ lab.name }}</a
-          >
-          <div>
-            <div class="flex justify-center py-4">
-              <NuxtImg
-                v-if="lab.prof.picture"
-                class="rounded-full"
-                :src="`/labs/${lab.prof.picture}`"
-                :alt="lab.prof.name.join(' ')"
-              />
-            </div>
-            <p class="text-center text-l">
-              Prof. {{ lab.prof.name.join(" ") }}
-              <br />
-              <a
-                class="underline text-[#212121] hover:text-[#ff0000] decoration-[#ff0000] hover:decoration-[#212121] text-xl"
-                :href="'mailto:' + lab.prof.email"
-                ><FontAwesomeIcon :icon="faEnvelope" class="text-[#707070]" /></a
-              ><br />
-            </p>
+          <p class="text-xs py-4">This page was last edited on {{ lastEdited.toDateString() }}.</p>
+          <div class="flex items-center justify-center">
+            <img :alt="project.name" :src="project.logo" class="p-4 object-contain w-full h-48" />
+          </div>
+          <ProjectStatus :project="project" />
+          <ProjectMaturity class="flex justify-end px-4" :maturity="project.maturity ? project.maturity : 0" />
+          <ProjectDescription :project="project" />
+          <div class="flex items-center space-x-2 text-left">
+            <FontAwesomeIcon :icon="faTags" class="text-gray-500" />
+            <span
+              v-for="tag in project.tags"
+              :key="tag"
+              class="px-3 py-1 rounded-full text-sm bg-[#d5d5d5] text-[#707070]"
+            >
+              {{ tag }}
+            </span>
           </div>
         </div>
-        <div class="py-4 text-left text-sm" v-html="lab.description" />
+        <ProjectsTabs v-if="tabs.length" :tabs="tabs" />
       </div>
+      <div class="flex-[3]">
+        <div class="px-8 bg-[#e6e6e6] text-center">
+          <div class="py-4">
+            <h1 class="text-4xl font-bold">{{ project.lab.name }}</h1>
+            <a
+              class="underline text-[#212121] hover:text-[#ff0000] decoration-[#ff0000] hover:decoration-[#212121] text-sm"
+              :href="lab.url"
+              >{{ lab.name }}</a
+            >
+            <div>
+              <div class="flex justify-center py-4">
+                <NuxtImg
+                  v-if="lab.prof.picture"
+                  class="rounded-full"
+                  :src="`/labs/${lab.prof.picture}`"
+                  :alt="lab.prof.name.join(' ')"
+                />
+              </div>
+              <p class="text-center text-l">
+                Prof. {{ lab.prof.name.join(" ") }}
+                <br />
+                <a
+                  class="underline text-[#212121] hover:text-[#ff0000] decoration-[#ff0000] hover:decoration-[#212121] text-xl"
+                  :href="'mailto:' + lab.prof.email"
+                  ><FontAwesomeIcon :icon="faEnvelope" class="text-[#707070]" /></a
+                ><br />
+              </p>
+            </div>
+          </div>
+          <div class="py-4 text-left text-sm" v-html="lab.description" />
+        </div>
+      </div>
+    </div>
+    <div class="flex justify-center pb-16">
+      <NuxtLink to="/"
+        ><span class="bg-[#ff0000] font-bold text-[#ffffff] px-4 py-2 cursor-pointer hover:bg-[#b51f1f]"
+          >Go back</span
+        ></NuxtLink
+      >
     </div>
   </div>
 </template>
