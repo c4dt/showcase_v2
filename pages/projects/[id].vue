b16c7ceb89988126df1308c51e9e54dca1fd0b4b
Carine Dengler <git@carine-dengler.de>
2025-06-13 17:59:09 +0200

    feat: do not center text

--- pages/projects/[id].vue
+++ pages/projects/[id].vue
@@ -9,7 +9,7 @@ const lastEdited = new Date(Date.parse(project.value.date_updated || project.val
 <template>
   <div>
     <div class="flex m-16">
-      <div class="pr-16 flex-[7] text-center">
+      <div class="pr-16 flex-[7]">
         <div class="py-4">
           <div class="flex items-center justify-center">
             <img :alt="project.name" :src="project.logo" class="p-4 object-contain w-full h-48" />
