3cddd17b9ba1d1cd72496796f6e95525b574c08e
Carine Dengler <git@carine-dengler.de>
2025-06-25 15:04:14 +0200

    fix: remove unnecessary style class

--- pages/projects/[id].vue
+++ pages/projects/[id].vue
@@ -24,7 +24,7 @@ const lastEdited = new Date(Date.parse(project.value.date_updated || project.val
             <ProjectQuality :project="project" />
           </div>
         </div>
-        <div class="flex items-center space-x-2 text-left">
+        <div class="flex items-center space-x-2">
           <span v-for="tag in project.tags.toSorted()" :key="tag" class="epfl-tag-light">
             {{ tag }}
           </span>
