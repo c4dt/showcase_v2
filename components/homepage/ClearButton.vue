b16a6eae6338b06f09bcdecc61a1feacec0fb708
Carine Dengler <git@carine-dengler.de>
2025-07-22 11:33:13 +0200

    fix: styling for positioning should be on parent container not component

--- components/homepage/ClearButton.vue
+++ components/homepage/ClearButton.vue
@@ -8,11 +8,7 @@ defineProps<{
 </script>
 
 <template>
-  <button
-    class="absolute inset-y-0 right-6 flex items-center"
-    aria-label="Clear all selections"
-    @click.prevent="clearFunc"
-  >
+  <button aria-label="Clear all selections" @click.prevent="clearFunc">
     <span class="epfl-times">
       <font-awesome :icon="['fas', 'times']" class="h-3 w-3" />
     </span>
--- components/homepage/DropdownButton.vue
+++ components/homepage/DropdownButton.vue
@@ -5,11 +5,7 @@ function toggleDropdown() {
 }
 </script>
 <template>
-  <button
-    class="absolute inset-y-0 right-0 flex items-center pr-2 text-gray-500"
-    aria-label="Toggle dropdown"
-    @click="toggleDropdown"
-  >
+  <button class="text-gray-500" aria-label="Toggle dropdown" @click="toggleDropdown">
     <font-awesome :icon="['fas', 'caret-down']" />
   </button>
 </template>
