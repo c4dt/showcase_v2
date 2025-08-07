7aa850beb9cc17b6b174cbba359336a3f3afd421
Carine Dengler <git@carine-dengler.de>
2025-08-07 15:46:16 +0200

    refactor: use Font Awesome search icon

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 const searchQuery = defineModel<string>("searchQuery");
 const searchInput = defineModel<HTMLInputElement | null>("searchInput");
 
@@ -16,19 +18,7 @@ const clearSearch = () => {
       type="text"
       class="w-full rounded-full bg-gray-200 py-2 pr-4 pl-10 text-gray-700 transition ring-inset focus:bg-white focus:ring-2 focus:ring-blue-300"
     />
-    <svg
-      class="absolute top-1/2 left-3 h-5 w-5 -translate-y-1/2 text-gray-500"
-      fill="none"
-      stroke="currentColor"
-      viewBox="0 0 24 24"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-      />
-    </svg>
+    <FontAwesomeIcon :icon="faMagnifyingGlass" class="absolute top-1/2 left-3 h-5 w-5 -translate-y-1/2 text-gray-500" />
     <!-- Clear Button -->
     <ClearButton
       v-if="searchQuery"
