16c0288e1b4be1ad434b1e357304bc39be810f58
Carine Dengler <git@carine-dengler.de>
2025-08-15 15:23:37 +0200

    feat: focus input element on toggle

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { nextTick } from "vue";
 const searchQuery = defineModel<string>("searchQuery");
 const searchInput = defineModel<HTMLInputElement | null>("searchInput");
 const { search, toggleSearch } = useSearch();
@@ -9,12 +10,17 @@ const clearSearch = () => {
   searchQuery.value = "";
   searchInput.value?.focus();
 };
+const toggleAndFocus = async () => {
+  toggleSearch();
+  await nextTick(); // wait for DOM update
+  searchInput.value?.focus();
+};
 </script>
 
 <template>
   <!-- use fixed-height container to maintain positioning of other elements when input is toggled off -->
   <div class="flex h-10">
-    <button @click="toggleSearch">
+    <button @click="toggleAndFocus">
       <FontAwesomeIcon :icon="faMagnifyingGlass" class="pr-2 hover:text-[#ff0000]" />
     </button>
     <div v-show="search" class="epfl-input flex items-center px-2">
