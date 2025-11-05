1bfc000178469979e10e64b241b83782c82e7f65
Carine Dengler <git@carine-dengler.de>
2025-11-05 18:00:51 +0100

    test: add minimal data for e2e test environment

--- e2e/navbar.spec.ts
+++ e2e/navbar.spec.ts
@@ -13,14 +13,14 @@ test.describe("breadcrumbs", () => {
   });
 
   test("contains project page breadcrumbs", async ({ page }) => {
-    await page.goto("/projects/dela");
+    await page.goto("/projects/eid-demo");
     const breadcrumbs = page.getByRole("navigation", { name: "Breadcrumb" }).getByRole("list");
     // on project page, home page breadcrumb IS a link
     const showcaseBreadcrumb = breadcrumbs.getByText("Showcase", { exact: true });
     await expect(showcaseBreadcrumb).toBeVisible();
     await expect(showcaseBreadcrumb).toHaveAttribute("href", "/");
     // on project page, project breadcrumb is NOT a link
-    const projectBreadcrumb = breadcrumbs.getByText("Dela", { exact: true });
+    const projectBreadcrumb = breadcrumbs.getByText("Eid Demo", { exact: true });
     await expect(projectBreadcrumb).toBeVisible();
     await expect(projectBreadcrumb).not.toHaveAttribute("href");
   });
