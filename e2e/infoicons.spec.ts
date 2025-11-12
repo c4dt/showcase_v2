e956d9872bdade1e55924f2b2363009eb94956f9
Carine Dengler <git@carine-dengler.de>
2025-11-12 10:39:23 +0100

    test: add e2e tests for `InfoIcons` component w/ project w/ full information

--- e2e/infoicons.spec.ts
+++ e2e/infoicons.spec.ts
@@ -49,3 +49,46 @@ test.describe("bare-bone project", () => {
     expect((await Promise.all(icons.map((icon) => icon.isVisible()))).filter((icon) => icon)).toHaveLength(1);
   });
 });
+
+test.describe("project w/ complete information", () => {
+  let projectCard;
+  let infoIcons;
+  test.beforeEach(async ({ page }) => {
+    projectCard = page.locator("[data-testid='project-card']").filter({ hasText: "E-ID Example" });
+    infoIcons = page.locator("[data-testid='info-icons']");
+    await page.goto("/");
+  });
+
+  test("navigates to project page's 'Research papers' section", async ({ page, baseURL }) => {
+    const icons = await projectCard.locator(infoIcons.locator(page.getByLabel("Papers"))).all();
+    await (await Promise.all(icons.filter((icon) => icon.isVisible())))[0].click();
+    await expect(page).toHaveURL(`${baseURL}/projects/eid-example?section=papers`);
+  });
+
+  test("navigates to project page's 'Miscellaneous publications' section", async ({ page, baseURL }) => {
+    const icons = await projectCard.locator(infoIcons.locator(page.getByLabel("Articles"))).all();
+    await (await Promise.all(icons.filter((icon) => icon.isVisible())))[0].click();
+    await expect(page).toHaveURL(`${baseURL}/projects/eid-example?section=articles`);
+  });
+
+  test("renders link to home page", async ({ page }) => {
+    const icons = await projectCard.locator(infoIcons.locator(page.getByLabel("Project Homepage"))).all();
+    const icon = (await Promise.all(icons.filter((icon) => icon.isVisible())))[0];
+    // do not test external URL, check only that link is present
+    await expect(icon).toHaveAttribute("href", "https://example.com");
+  });
+
+  test("renders link w/ code icon", async ({ page }) => {
+    const icons = await projectCard.locator(infoIcons.locator(page.getByLabel("Source Code"))).all();
+    const icon = (await Promise.all(icons.filter((icon) => icon.isVisible())))[0];
+    // do not test external URL, check only that link is present
+    await expect(icon).toHaveAttribute("href", "https://example.com/code");
+  });
+
+  test("renders mailto link", async ({ page }) => {
+    const icons = await projectCard.locator(infoIcons.locator(page.getByLabel("Contact"))).all();
+    const icon = (await Promise.all(icons.filter((icon) => icon.isVisible())))[0];
+    // check that link is present
+    await expect(icon).toHaveAttribute("href", "mailto:linus.gasser@epfl.ch");
+  });
+});
