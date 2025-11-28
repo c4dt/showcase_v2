090250fc256e976e2e01b539d968a4e562360796
Carine Dengler <git@carine-dengler.de>
2025-11-28 10:55:59 +0100

    test: add e2e tests for SelectedProjects component

---
+++ e2e/selectedProjects.spec.ts
@@ -0,0 +1,102 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("bare-bone project", () => {
+  test.describe.configure({ retries: 6 }); // tests are flaky for chromium/webkit
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/");
+  });
+
+  test("navigates to project page when clicked", async ({ page, baseURL }) => {
+    // multiple carousel items are generated per project for the carousel functionality
+    const carouselItems = await page
+      .locator("[data-testid='carousel-item']")
+      .filter({ hasText: "E-ID Demo (Issuer & Verifier)" })
+      .all();
+    // choose visible carousel item
+    const carouselItem = (await Promise.all(carouselItems.filter((item) => item.isVisible())))[0];
+    await carouselItem.click();
+    // no additional information is associated with the project, so no tab is open
+    await expect(page).toHaveURL(`${baseURL}/projects/eid-demo`);
+  });
+
+  test("displays the project logo", async ({ page }) => {
+    // multiple carousel items are generated per project for the carousel functionality
+    const carouselItems = await page
+      .locator("[data-testid='carousel-item']")
+      .filter({ hasText: "E-ID Demo (Issuer & Verifier)" })
+      .all();
+    // choose visible carousel item
+    const carouselItem = (await Promise.all(carouselItems.filter((item) => item.isVisible())))[0];
+    // default logo
+    await expect(carouselItem.locator("img")).toHaveAttribute(
+      "src",
+      "https://c4dt.epfl.ch/wp-content/themes/epfl/assets/svg/epfl-logo.svg"
+    );
+  });
+
+  test("displays project description", async ({ page }) => {
+    // multiple carousel items are generated per project for the carousel functionality
+    const carouselItems = await page
+      .locator("[data-testid='carousel-item']")
+      .filter({ hasText: "E-ID Demo (Issuer & Verifier)" })
+      .all();
+    // choose visible carousel item
+    const carouselItem = (await Promise.all(carouselItems.filter((item) => item.isVisible())))[0];
+    await expect(carouselItem).toContainText("Demo for the swiss E-ID ledger. It includes an issuer and a verifier.");
+  });
+
+  test("displays information icons", async ({ page }) => {
+    // multiple carousel items are generated per project for the carousel functionality
+    const carouselItems = await page
+      .locator("[data-testid='carousel-item']")
+      .filter({ hasText: "E-ID Demo (Issuer & Verifier)" })
+      .all();
+    // choose visible carousel item
+    const carouselItem = (await Promise.all(carouselItems.filter((item) => item.isVisible())))[0];
+    await expect(carouselItem.locator(page.locator("[data-testid='info-icons']"))).toBeVisible();
+  });
+});
+
+test.describe("project w/ complete information", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/");
+  });
+
+  test("navigates to project page when clicked", async ({ page, baseURL }) => {
+    // multiple carousel items are generated per project for the carousel functionality
+    const carouselItems = await page.locator("[data-testid='carousel-item']").filter({ hasText: "E-ID Example" }).all();
+    // choose visible carousel item
+    const carouselItem = (await Promise.all(carouselItems.filter((item) => item.isVisible())))[0];
+    await carouselItem.click();
+    // if there is no related information regarding C4DT's work, the default open tab is "Technical"
+    await expect(page).toHaveURL(`${baseURL}/projects/eid-example?section=technical`);
+  });
+
+  test("displays project logo", async ({ page }) => {
+    // multiple carousel items are generated per project for the carousel functionality
+    const carouselItems = await page.locator("[data-testid='carousel-item']").filter({ hasText: "E-ID Example" }).all();
+    // choose visible carousel item
+    const carouselItem = (await Promise.all(carouselItems.filter((item) => item.isVisible())))[0];
+    // custom logo
+    await expect(carouselItem.locator("img")).toHaveAttribute(
+      "src",
+      "https://raw.githubusercontent.com/spring-epfl/hippiepug/refs/heads/master/hippiepug.svg"
+    );
+  });
+
+  test("displays project description", async ({ page }) => {
+    // multiple carousel items are generated per project for the carousel functionality
+    const carouselItems = await page.locator("[data-testid='carousel-item']").filter({ hasText: "E-ID Example" }).all();
+    // choose visible carousel item
+    const carouselItem = (await Promise.all(carouselItems.filter((item) => item.isVisible())))[0];
+    await expect(carouselItem).toContainText("Example for the swiss E-ID ledger.");
+  });
+
+  test("displays information icons", async ({ page }) => {
+    // multiple carousel items are generated per project for the carousel functionality
+    const carouselItems = await page.locator("[data-testid='carousel-item']").filter({ hasText: "E-ID Example" }).all();
+    // choose visible carousel item
+    const carouselItem = (await Promise.all(carouselItems.filter((item) => item.isVisible())))[0];
+    await expect(carouselItem.locator(page.locator("[data-testid='info-icons']"))).toBeVisible();
+  });
+});
