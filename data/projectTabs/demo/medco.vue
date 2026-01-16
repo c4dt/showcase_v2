b501ba8a5816e75cb82b24a11f0ee1433c81093e
Linus Gasser <linus.gasser@epfl.ch>
2026-01-16 16:16:50 +0100

    Convert html project tabs to template
    The html project pages contained references to the images using showcase_v2 as the
    base url, but that doesn't work with local testing.
    According to Claude, the solution is to use NuxtImg, but this needs templates and not html.

---
+++ data/projectTabs/demo/medco.vue
@@ -0,0 +1,22 @@
+<template>
+  <h3>This project has been retired from the C4DT Factory Incubator</h3>
+  <p>
+    It is no longer actively supported. If you're interested or have questions, please get in contact with
+    <a class="epfl-a" href="https://tuneinsight.com">TuneInsight.</a>
+  </p>
+  <p>
+    Besides the documentation and source code of MedCo-Explore, we also provide a way to try out this software. To do
+    that just log in our <a class="epfl-a" href="https://medco.epfl.ch/demo/">live demo</a> using the correct
+    credentials. We are running MedCo-Explore with 3 different data providers (clinical sites) with 8,000 patients. For
+    test purposes the data is synthetically generated.
+  </p>
+
+  <div class="click-zoom center">
+    <label>
+      <NuxtImg src="/products/images/medco/demo.png" width="50%" />
+      <br />The query building in medco
+    </label>
+  </div>
+
+  <a class="epfl-a tab-link" href="https://medco-ch.github.io/"> For Demo Click Here </a>
+</template>
