90ca3930d9c91bcbcdf5a2c398487b974f8e6afc
Linus Gasser <linus.gasser@epfl.ch>
2026-03-23 16:07:20 +0100

    feat: Merging data/*/projects.yaml with showcase/data

---
+++ data/projectTabs/presentation/dvoting.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>
+    The D-Voting project allows for electronic voting without a central point of trust. It relies instead on the
+    majority of servers to be honest during the voting process. The ballots are encrypted during the voting phase, and
+    then shuffled to anonymise the results. When they are finally decrypted, it is not possible anymore to retrace who
+    voted for what.
+  </p>
+  <p>
+    In Summer 2024, the D-Voting system was used for the first time on a bigger scale to elect the members of the EPFL
+    school assembly. A little bit over 1000 voters used the system to elect the members, and after the resolution of a
+    bug discovered during the on-going voting process, the elections could be successfully concluded. Since then the
+    system has been used multiple times for smaller elections or to poll the EPFL community on administrative questions.
+  </p>
+  <p>In 2025, the system was used successfully in the election of the school assembly.</p>
+</template>
